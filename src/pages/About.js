2b579b795041ddbe9a51eec8fafd756dd9a22b3f
hepohle <hectorpohle@gmail.com>
2021-07-26 11:07:57 -0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+      Tumeric knausgaard gluten-free, health goth af lyft activated charcoal literally shaman fingerstache four loko butcher banjo letterpress. Lomo farm-to-table tofu live-edge bushwick kale chips palo santo lyft. Poke copper mug brooklyn pickled, farm-to-table portland put a bird on it mumblecore ethical. Cloud bread pug waistcoat, pabst gluten-free bespoke schlitz direct trade dreamcatcher put a bird on it viral everyday carry. Flexitarian typewriter ramps la croix PBR&B, drinking vinegar lomo sustainable dreamcatcher cloud bread.
+      </p>
+    </section>
+  )
+}
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CocktailList from "../components/CocktailList";
+import SearchForm from "../components/SearchForm";
+
+export default function Home() {
+  const [loading,setLoading] = React.useState(false);
+  const [searchTerm,setSearchTerm] = React.useState('a');
+  const [cocktails,setCocktails] = React.useState([]);
+  return (
+    <main>
+      <SearchForm setSearchTerm={setSearchTerm} />
+      <CocktailList loading={loading} cocktails={cocktails} />
+    </main>
+  )
+}
